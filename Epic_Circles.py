6c7a9e7918347fdafecafaf19977d7d45e18c51b
JamisonThorne <jamison.e.thorne@gmail.com>
2024-04-13 17:25:30 -0500

    Commit with complex descartes theorem

--- Epic_Circles.py
+++ Epic_Circles.py
@@ -3,106 +3,70 @@ import matplotlib.pyplot as plt
 import math 
 from fraction import Fraction
 
-def main():
+def main(arg_1):
     # Define a radius for the first circle, after that everything else is taken care of. Enjoy :)
     decartes_circles = []
-    radius_n = 1    # radius of first circle
-    origin_x = 0    # origin (center of first circle) in this case will always be at 0,0
-    origin_y = 0    # origin (center of first circle) in this case will always be at 0,0
-    ###############################################
-    graph_range = 1
-    # Figure
+    graph_range = arg_1
+    # Figure Generation
     fig, ax = plt.subplots() # note we must use plt.subplots, not plt.subplot
     ax.cla() # clear things for fresh plot
     # change default range so that new circles will work
     ax.set_xlim((-1*graph_range, graph_range))
     ax.set_ylim((-1*graph_range, graph_range))
+    #   Initial declaration of the first three circles, after this decartes will take over
+    decartes_circles.append([0,0,arg_1]) #  Define first circle
+    decartes_circles.append([0,arg_1/2,arg_1/2]) #  Define first circle
+    decartes_circles.append([0,arg_1/2-1,arg_1/2]) #  Define first circle
     ###############################################
-    # Drawing Initial Circle
-    circle = plt.Circle((origin_x, origin_y), radius_n, color='r',fill=False)
-    ax.add_patch(circle)
-    #################################
-    #   First Circle
-    next_circle_x = origin_x
-    next_circle_y = origin_y + radius_n/2
-    circle = plt.Circle((next_circle_x, next_circle_y), radius_n/2, color='blue',fill=False)
-    ax.add_patch(circle)
-    #################################
-    #   Second Circle
-    next_circle_x = origin_x
-    next_circle_y = origin_y - radius_n/2
-    circle = plt.Circle((next_circle_x, next_circle_y), radius_n/2, color='blue',fill=False)
-    ax.add_patch(circle)
-    #################################
-    #   Descartes
-    decartes_circles.append(Descartes_Theorem(0.5,0.5,-1))
-    #################################
-    #   Third Circle
-    next_circle_x = 1-decartes_circles[0]
-    third_circle_radius = decartes_circles[0]
-    next_circle_y = 0
-    circle = plt.Circle((next_circle_x, next_circle_y), decartes_circles[0], color='blue',fill=False)
-    ax.add_patch(circle)
-    #################################
-    #   Fourth Circle 
-    next_circle_x = decartes_circles[0]-1
-    next_circle_y = 0
-    circle = plt.Circle((next_circle_x, next_circle_y), decartes_circles[0], color='blue',fill=False)
-    ax.add_patch(circle)
-    #################################
-    #   Descartes
-    decartes_circles.append(Descartes_Theorem(decartes_circles[0],0.5,0.5))
-    #################################
-    #   Kissing Circle 
-    next_circle_x = -1*radius_n + third_circle_radius + third_circle_radius + decartes_circles[1]
-    next_circle_y = 0
-    circle = plt.Circle((next_circle_x, next_circle_y), decartes_circles[1], color='blue',fill=True)
-    ax.add_patch(circle)
-    #################################
-    #   Kissing Circle 
-    next_circle_x = 1*radius_n - third_circle_radius - third_circle_radius - decartes_circles[1]
-    next_circle_y = 0
-    circle = plt.Circle((next_circle_x, next_circle_y), decartes_circles[1], color='blue',fill=True)
-    ax.add_patch(circle)
-    #################################
-    #   At this point, the next circles to be found need to be automated or this could get painful.
-    #   Probably after automation of next steps, the beginning can be automated
-    #################################
-    #   Descartes
-    decartes_circles.append(Descartes_Theorem(decartes_circles[0],0.5,-1))
+    for i in range(0,2):
+        if i==0:
+            temp_r = Descartes_Theorem(-1*decartes_circles[i][2],decartes_circles[i+1][2],decartes_circles[i+2][2])
+            [temp_x,temp_y] = Complex_Descartes_Theorem(
+                decartes_circles[i][0],decartes_circles[i][1],-1*decartes_circles[i][2],
+                decartes_circles[i+1][0],decartes_circles[i+1][1],decartes_circles[i+1][2],
+                decartes_circles[i+2][0],decartes_circles[i+2][1],decartes_circles[i+2][2],
+                temp_r)
+            if abs(temp_x) == abs(temp_y):
+                temp_y = 0
+            decartes_circles.append([temp_x,temp_y,temp_r])
+        else:
+            temp_r = Descartes_Theorem(decartes_circles[i][2],decartes_circles[i+1][2],decartes_circles[i+2][2])
+            [temp_x,temp_y] = Complex_Descartes_Theorem(
+                decartes_circles[i][0],decartes_circles[i][1],decartes_circles[i][2],
+                decartes_circles[i+1][0],decartes_circles[i+1][1],decartes_circles[i+1][2],
+                decartes_circles[i+2][0],decartes_circles[i+2][1],decartes_circles[i+2][2],
+                temp_r)
+            if abs(temp_x) == abs(temp_y):
+                temp_y = 0
+            decartes_circles.append([temp_x,temp_y,temp_r])
+            
+    for i in range(0,len(decartes_circles)):
+        ###############################################
+        # Drawing Initial Circle
+        circle = plt.Circle((decartes_circles[i][0], decartes_circles[i][1]), decartes_circles[i][2], color='r',fill=False)
+        ax.add_patch(circle)
+    plt.show()
     1
-    # [x0,y0,x1,y1] = intersection_of_two_circles(next_circle_x,next_circle_y,radius_n/2,origin_x,origin_y,radius_n/2)
-    # [a1, b1] = equation_of_a_line(next_circle_x,next_circle_y,x0,y0) # Find the equation of a line going from point of intersection to center
-    # plt.axline((next_circle_x, next_circle_y), (x0, y0))
-    # plt.axline((next_circle_x, next_circle_y), (x1, y1))
-    # plt.plot(next_circle_x, next_circle_y, x0, y0, marker = 'o')
-    # plt.plot(next_circle_x, next_circle_y, x1, y1, marker = 'o')
     
-    # next_circle_x = origin_x - 0.5
-    # next_circle_y = origin_y
-    # # circle = plt.Circle((next_circle_x, next_circle_y), radius_n/2, color='blue',fill=False)
-    # # ax.add_patch(circle)
-    # plt.plot(-(b0/a0), 0, x1,0, marker = 'o')
-    # plt.plot(-1-x1, 0, marker = 'o')
-    # plt.plot((-(b0/a0))-x1/2, 0, marker = '^')
-    # temp = (-(b0/a0))-x1/2
-    # test1 = -1-temp
-    # dx = x1 - temp
-    # dy = y1
-    # test2 = d = math.sqrt(dx**2 + dy**2)
-    # 1
-    
-    
-    # plt.show()
-    # [a0, b0] = equation_of_a_line(next_circle_x,next_circle_y,x0,y0) # Find the equation of a line going from point of intersection to center
-    # equation_of_a_line(next_circle_x,next_circle_y,x1,y1) # Find the equation of a line going from point of intersection to center
-    # 1
 
-def Complex_Descartes_Theorem(r1,r2,r3):
+def Complex_Descartes_Theorem(x1,y1,r1,x2,y2,r2,x3,y3,r3,r4):
     #   Calculates the center of the tangential circle:
     #       z4 = z1*k1+z2*k2+z3*k3+/-2(root(k1*k2*z1*z2+k2*k3*z2*z3+k1*k3*z1*z3))/k4
     #   Where k is radius or circles
     #   z is the complex number of x,y (center coordinates or circle) where z = x + iy
+    k1 = 1/r1
+    k2 = 1/r2
+    k3 = 1/r3
+    k4 = 1/r4
+    z1 = complex(x1,y1)
+    z2 = complex(x2,y2)
+    z3 = complex(x3,y3)
+    first_value = z1*k1 + z2*k2 + z3*k3
+    second_value = k1*k2*z1*z2 + k2*k3*z2*z3 + k1*k3*z1*z3
+    z4_x = (abs(first_value) - (2*math.sqrt(abs(second_value))))/k4
+    z4_y = (abs(first_value) + (2*math.sqrt(abs(second_value))))/k4
+    return z4_x,z4_y
+    
 def Descartes_Theorem(r1,r2,r3):
     k1 = 1/r1
     k2 = 1/r2
@@ -147,4 +111,4 @@ def PointsInCircum(r,number_of_points):
     return [(math.cos(2*pi/number_of_points*x)*r,math.sin(2*pi/number_of_points*x)*r) for x in range(0,number_of_points+1)]
 
 if __name__ == '__main__':
-    main()
+    main(1)
